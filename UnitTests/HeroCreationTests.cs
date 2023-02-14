0c5baed328dcd730fb4de0742bbe96a78f16eba7
rene-marcker <rene-marcker@hotmail.com>
2023-02-14 14:51:19 +0100

    fix: make the creation Class unit test more simply

--- UnitTests/HeroCreationTests.cs
+++ UnitTests/HeroCreationTests.cs
@@ -1,7 +1,5 @@
 using ConsoleRPG.Heroes;
 using ConsoleRPG.Items;
-using System.Security.Cryptography.X509Certificates;
-using Xunit;
 
 namespace UnitTests
 {
@@ -14,38 +12,12 @@ namespace UnitTests
             //Arrange
             string name = "Gandalf";
             Mage whiteMage = new Mage(name);
-            List<WeaponType> weapons= new List<WeaponType>()
-            {
-                WeaponType.Staff,
-                WeaponType.Wand,
-            };
-            List<ArmorType> armors = new List<ArmorType>()
-            {
-                ArmorType.Cloth
-            };
-            object[] expected = new object[]
-            {
-                name,
-                1,
-                1,
-                1,
-                8,
-                weapons,
-                armors
-            };
-            //Act
-            object[] actual = new object[]
-            {
-                whiteMage.Name,
-                whiteMage.Level,
-                whiteMage.HeroAttributes.Strength,
-                whiteMage.HeroAttributes.Dexterity,
-                whiteMage.HeroAttributes.Intelligence,
-                whiteMage.ValidWeaponTypes,
-                whiteMage.ValidArmorTypes
-            };
-            //Assert
-            Assert.Equal(expected,actual);
+            //Act and Assert
+            Assert.Equal(name, whiteMage.Name);
+            Assert.Equal(1, whiteMage.Level);
+            Assert.Equal(1, whiteMage.HeroAttributes.Strength);
+            Assert.Equal(1, whiteMage.HeroAttributes.Dexterity);
+            Assert.Equal(8, whiteMage.HeroAttributes.Intelligence);
         }
         #endregion
 
@@ -55,39 +27,13 @@ namespace UnitTests
         {
             //Arrange
             string name = "Legolas";
-            Ranger elverPrince = new Ranger(name);
-            List<WeaponType> weapons = new List<WeaponType>()
-            {
-                WeaponType.Bow,
-            };
-            List<ArmorType> armors = new List<ArmorType>()
-            {
-                ArmorType.Leather,
-                ArmorType.Mail,
-            };
-            object[] expected = new object[]
-            {
-                name,
-                1,
-                1,
-                7,
-                1,
-                weapons,
-                armors
-            };
-            //Act
-            object[] actual = new object[]
-            {
-                elverPrince.Name,
-                elverPrince.Level,
-                elverPrince.HeroAttributes.Strength,
-                elverPrince.HeroAttributes.Dexterity,
-                elverPrince.HeroAttributes.Intelligence,
-                elverPrince.ValidWeaponTypes,
-                elverPrince.ValidArmorTypes
-            };
-            //Assert
-            Assert.Equal(expected, actual);
+            Ranger elvenPrince = new Ranger(name);
+            //Act and Assert
+            Assert.Equal(name, elvenPrince.Name);
+            Assert.Equal(1, elvenPrince.Level);
+            Assert.Equal(1, elvenPrince.HeroAttributes.Strength);
+            Assert.Equal(7, elvenPrince.HeroAttributes.Dexterity);
+            Assert.Equal(1, elvenPrince.HeroAttributes.Intelligence);
         }
         #endregion
 
@@ -97,40 +43,13 @@ namespace UnitTests
         {
             //Arrange
             string name = "Gwaine";
-            Rogue merlinKnight = new Rogue(name);
-            List<WeaponType> weapons = new List<WeaponType>()
-            {
-                WeaponType.Dagger,
-                WeaponType.Sword,
-            };
-            List<ArmorType> armors = new List<ArmorType>()
-            {
-                ArmorType.Leather,
-                ArmorType.Mail,
-            };
-            object[] expected = new object[]
-            {
-                name,
-                1,
-                2,
-                6,
-                1,
-                weapons,
-                armors
-            };
-            //Act
-            object[] actual = new object[]
-            {
-                merlinKnight.Name,
-                merlinKnight.Level,
-                merlinKnight.HeroAttributes.Strength,
-                merlinKnight.HeroAttributes.Dexterity,
-                merlinKnight.HeroAttributes.Intelligence,
-                merlinKnight.ValidWeaponTypes,
-                merlinKnight.ValidArmorTypes
-            };
-            //Assert
-            Assert.Equal(expected, actual);
+            Rogue knight = new Rogue(name);
+            //Act and Assert
+            Assert.Equal(name, knight.Name);
+            Assert.Equal(1, knight.Level);
+            Assert.Equal(2, knight.HeroAttributes.Strength);
+            Assert.Equal(6, knight.HeroAttributes.Dexterity);
+            Assert.Equal(1, knight.HeroAttributes.Intelligence);
         }
         #endregion
 
@@ -141,40 +60,12 @@ namespace UnitTests
             //Arrange
             string name = "Arthur";
             Warrior king = new Warrior(name);
-            List<WeaponType> weapons = new List<WeaponType>()
-            {
-                WeaponType.Axe,
-                WeaponType.Hammer,
-                WeaponType.Sword,
-            };
-            List<ArmorType> armors = new List<ArmorType>()
-            {
-                ArmorType.Mail,
-                ArmorType.Plate
-            };
-            object[] expected = new object[]
-            {
-                name,
-                1,
-                5,
-                2,
-                1,
-                weapons,
-                armors
-            };
-            //Act
-            object[] actual = new object[]
-            {
-                king.Name,
-                king.Level,
-                king.HeroAttributes.Strength,
-                king.HeroAttributes.Dexterity,
-                king.HeroAttributes.Intelligence,
-                king.ValidWeaponTypes,
-                king.ValidArmorTypes
-            };
-            //Assert
-            Assert.Equal(expected, actual);
+            //Act and Assert
+            Assert.Equal(name, king.Name);
+            Assert.Equal(1, king.Level);
+            Assert.Equal(5, king.HeroAttributes.Strength);
+            Assert.Equal(2, king.HeroAttributes.Dexterity);
+            Assert.Equal(1, king.HeroAttributes.Intelligence);
         }
         #endregion
 
--- UnitTests/LevelTests.cs
+++ UnitTests/LevelTests.cs
@@ -1,8 +1,44 @@
 ﻿
 
+using ConsoleRPG.Heroes;
+
 namespace UnitTests
 {
     public class LevelTests
     {
+        #region Level-up Mage
+        [Fact]
+        public void LevelUp_MageToLevel2_ShouldReturnLevel2MageAttributes()
+        {
+            //Arrange
+            Mage whiteMage = new Mage("Gandalf");
+
+        }
+        #endregion
+        
+        #region Level-up Ranger
+        [Fact]
+        public void LevelUp_RangerToLevel2_ShouldReturnLevel2RangerAttributes()
+        {
+            Ranger elvenPrince = new Ranger("Legolas");
+        }
+
+        #endregion
+
+        #region Level-up Rogue
+        [Fact]
+        public void LevelUp_RogueToLevel2_ShouldReturnLevel2RogueAttributes()
+        {
+            Rogue knight = new Rogue("Gwaine");
+        }
+        #endregion
+
+        #region Level-up Warrior
+        [Fact]
+        public void LevelUp_WarriorToLevel2_ShouldReturnLevel2WarriorAttributes()
+        {
+            Warrior king = new Warrior("Arthur");
+        }
+        #endregion
     }
 }
