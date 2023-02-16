fa1b6541d7783021412be8d7a6a11576d1dbe4f7
rene-marcker <rene-marcker@hotmail.com>
2023-02-16 14:27:15 +0100

    fix: removed set methods for fields in HeroAttributes... no longer useable after override methods.

--- Heroes/HeroAttributes.cs
+++ Heroes/HeroAttributes.cs
@@ -6,13 +6,6 @@
         private int _dexterity;
         private int _intelligence;
 
-
-
-        public int Strength { get => _strength; set => _strength = value; }
-        public int Dexterity { get => _dexterity; set => _dexterity = value; }
-        public int Intelligence { get => _intelligence; set => _intelligence = value; }
-
-
         public HeroAttributes(int strength, int dexterity, int intelligence)
         {
             _strength = strength;
