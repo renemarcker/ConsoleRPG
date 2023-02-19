fca50d84b9376c6c07cbe24bd936d0272c2181ab
rene-marcker <rene-marcker@hotmail.com>
2023-02-19 11:05:45 +0100

    fixbug: fix the unit test with display... failed the test run on GitHub, as the damage is 1.05, but locally it is 1,05.

--- UnitTests/Hero/DisplayTests.cs
+++ UnitTests/Hero/DisplayTests.cs
@@ -14,13 +14,14 @@ namespace UnitTests.Hero
             //Arrange
             Warrior peasant = new("Rene");
             StringBuilder builder = new("The states of the Hero:\n\n");
+            decimal damage = 1.05m;
             builder.Append("Name:         Rene\n");
             builder.Append("Class:        Warrior\n");
             builder.Append("Level:        1\n");
             builder.Append("Strength:     5\n");
             builder.Append("Dexterity:    2\n");
             builder.Append("Intelligence: 1\n");
-            builder.Append("Damage:       1,05\n");
+            builder.AppendFormat("Damage:       {0}\n", damage);
             string expected = builder.ToString();   
             //Act
             string actual = peasant.Display();
